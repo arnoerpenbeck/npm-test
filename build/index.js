fd1a8cec9a75cd72b4ea4c117c905b75261285ca
Arno Erpenbeck <88486704+arnoerpenbeck@users.noreply.github.com>
2024-06-28 08:37:19 +0200

    fix build

--- build/index.js
+++ build/index.js
@@ -1,6 +1,6 @@
 "use strict";
 const sayHello = (subject) => {
-    console.log('Hello ' + subject);
+    console.log('Hello, ' + subject);
 };
-sayHello('TypeScript');
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBLE1BQU0sUUFBUSxHQUFHLENBQUMsT0FBZSxFQUFRLEVBQUU7SUFDekMsT0FBTyxDQUFDLEdBQUcsQ0FBQyxRQUFRLEdBQUcsT0FBTyxDQUFDLENBQUM7QUFDbEMsQ0FBQyxDQUFDO0FBRUYsUUFBUSxDQUFDLFlBQVksQ0FBQyxDQUFDIn0=
+sayHello('TypeScript!');
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBLE1BQU0sUUFBUSxHQUFHLENBQUMsT0FBZSxFQUFRLEVBQUU7SUFDekMsT0FBTyxDQUFDLEdBQUcsQ0FBQyxTQUFTLEdBQUcsT0FBTyxDQUFDLENBQUM7QUFDbkMsQ0FBQyxDQUFDO0FBRUYsUUFBUSxDQUFDLGFBQWEsQ0FBQyxDQUFDIn0=
